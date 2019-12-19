15d912f459bbedcc89512a022c9708d6f524eb72
viniciuszendron <vinicius.zendron@meantrix.com>
2019-12-19 16:42:38 -0300

    Revert "dryRun added"
    This reverts commit 978e64f6108244edb3b01095e862986b3ee774ee.

--- R/bq-perform.R
+++ R/bq-perform.R
@@ -249,7 +249,6 @@ bq_perform_query <- function(query, billing,
                              create_disposition = "CREATE_IF_NEEDED",
                              write_disposition = "WRITE_EMPTY",
                              use_legacy_sql = FALSE,
-                             dry_run = FALSE,
                              priority = "INTERACTIVE"
                              ) {
   assert_that(is.string(query), is.string(billing))
@@ -257,7 +256,6 @@ bq_perform_query <- function(query, billing,
   query <- list(
     query = unbox(query),
     useLegacySql = unbox(use_legacy_sql),
-    dryRun = unbox(dry_run),
     priority = unbox(priority)
   )
 
